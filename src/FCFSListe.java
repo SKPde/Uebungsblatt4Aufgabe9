c6fa3e6a78e358dc9060f3edda0f502d9137edc9
s.kuebel <s.kuebel@otterbach.de>
2017-08-04 16:58:02 +0200

    Erster und Letzter Patient können nun gespeichert werden. Eine Kette wird aufgebaut.

--- src/FCFSListe.java
+++ src/FCFSListe.java
@@ -1,10 +1,20 @@
 
 public class FCFSListe implements WarteSchlange{
-
+	
+	public ZPatient anfang;
+	public ZPatient ende;
+	
+	
 	@Override
 	public void anmelden(ZPatient p) {
 		// TODO Auto-generated method stub
-		
+		if(anfang == null) {
+			anfang = p;
+			ende = p;
+		} else {
+			ende.next = p;
+			ende = p;
+		}
 	}
 
 	@Override
